8e817076c7250712976305864eb5e3e421b2f491
DanaiWee <Danai_wee@hotmail.com>
2025-06-25 16:02:27 +0700

    Implement LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+import LeftSidebar from "@/components/navigation/LeftSidebar";
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const loggedIn = { firstName: "Danai", lastName: "Wee" };
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className='flex h-screen w-full font-inter'>
+      <LeftSidebar user={loggedIn} />
+      {children}
     </main>
   );
 };
--- types/global.d.ts
+++ types/global.d.ts
@@ -11,6 +11,22 @@ declare global {
     appwriteItemId: string;
     sharableId: string;
   }
+
+  interface User {
+    $id: string;
+    email: string;
+    userId: string;
+    dwollaCustomerUrl: string;
+    dwollaCustomerId: string;
+    firstName: string;
+    lastName: string;
+    address1: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    dateOfBirth: string;
+    ssn: string;
+  }
 }
 
 export {};
